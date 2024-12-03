1299e30902de56d8fdb28ed239e8d4960452fee6
Hrushikesh Pathak <hrushikeshpathak@Hrushikeshs-MacBook-Pro.local>
2024-12-03 14:56:06 +0530

    gulf-03-01/--security headers

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  {
+    name: "generate-headers",
+    closeBundle() {
+      const headers = `
+/*
+  Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;
+  X-Frame-Options: SAMEORIGIN
+  Referrer-Policy: no-referrer
+  Permissions-Policy: geolocation=(), microphone=(), camera=()
+`;
+      fs.writeFileSync(path.resolve(__dirname, "dist/_headers"), headers);
+    },
+  },
+
+  ],
 })
