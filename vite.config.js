a59e2ee7741a67f8190e0dfb9ab8753482c544da
Hrushikesh Pathak <hrushikeshpathak@Hrushikeshs-MacBook-Pro.local>
2024-12-03 15:11:46 +0530

    gulf-03-01/0.2--security headers

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Define __dirname for ES Module compatibility
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "generate-headers",
-      closeBundle() {
-        const headers = `
-/*
-  Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;
-  X-Frame-Options: SAMEORIGIN
-  Referrer-Policy: no-referrer
-  Permissions-Policy: geolocation=(), microphone=(), camera=()
-`;
-        fs.writeFileSync(path.resolve(__dirname, "dist/_headers"), headers);
-      },
-    },
-  ],
-});
+  plugins: [react(),],
+})
